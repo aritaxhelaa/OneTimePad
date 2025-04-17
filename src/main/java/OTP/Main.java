0a65124bb8d76dcfa733b095227bf3002931413d
ArxhenteKadriu <149197219+Arxhenta@users.noreply.github.com>
2025-04-17 20:23:31 +0200

    updated main

--- src/main/java/OTP/Main.java
+++ src/main/java/OTP/Main.java
@@ -3,10 +3,23 @@ package OTP;
 import java.util.Base64;
 
 public class Main {
-public static void main(String[] args) {
-    String message = "Pershendetje, ky kod i dhene eshte i dekriptuar";
+    public static void main(String[] args) {
+        String message = "Pershendetje, ky kod eshte i dekriptuar";
+        String seed = "12345"; 
+        SymmetricAlgorithm otpEncrypt = new SymmetricAlgorithm(seed);
+        byte[] encryptedBytes = otpEncrypt.encrypt(message);
 
-    String seed = "12345";
-}
+        String encodedCipher = Base64.getEncoder().encodeToString(encryptedBytes);
+        System.out.println("Encrypted (Base64): " + encodedCipher);
+
+      
+        SymmetricAlgorithm otpDecrypt = new SymmetricAlgorithm(seed);
+        byte[] decodedCipher = Base64.getDecoder().decode(encodedCipher);
+        String decrypted = otpDecrypt.decrypt(decodedCipher);
+
+        System.out.println("Decrypted: " + decrypted);
+
+        FileSaver.saveToFile("encrypted_message.txt", encodedCipher);
+    }
 }
 
