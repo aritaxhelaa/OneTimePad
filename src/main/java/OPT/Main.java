f83d745bbbe4e57bd48bdec1e4a2b6e8804b7057
ArxhenteKadriu <149197219+Arxhenta@users.noreply.github.com>
2025-04-17 16:58:18 +0200

    added Main

--- src/main/java/OPT/Main.java
+++ src/main/java/OPT/Main.java
@@ -1,4 +1,12 @@
 package OPT;
 
+import java.util.Base64;
+
 public class Main {
+public static void main(String[] args) {
+    String message = "Pershendetje, ky kod i dhene eshte i dekriptuar";
+
+    String seed = "12345";
 }
+}
+
