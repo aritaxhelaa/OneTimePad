4f225e7d4e55db396644187dc97b44085a61e37a
BahrieVeseli <bahrie.veseli@student.uni-pr.edu>
2025-04-17 11:50:25 +0200

    Update MessageInput.java

--- src/main/java/OPT/MessageInput.java
+++ src/main/java/OPT/MessageInput.java
@@ -1,4 +1,17 @@
 package OPT;
-
+import java.util.Scanner;
 public class MessageInput {
-}
+
+    public static String getUserMessage() {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Shkruaj mesazhin për enkriptim: ");
+            return scanner.nextLine();
+        }
+
+        public static String getSeed() {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Shkruaj seed (int ose string): ");
+            return scanner.nextLine();
+        }
+    }
+
