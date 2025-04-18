799c54f6ed9155e9d98eec755cf7357d18cf1bc8
Arita <arita.xhela@student.uni-pr.edu>
2025-04-18 18:10:54 +0200

    Update SymmetricAlgorithm

--- src/main/java/OTP/SymmetricAlgorithm.java
+++ src/main/java/OTP/SymmetricAlgorithm.java
@@ -1,4 +1,57 @@
 package OTP;
 
+import java.util.Random;
+import java.util.Base64;
+
 public class SymmetricAlgorithm {
+    private Random keyGenerator;
+
+    public SymmetricAlgorithm(Object seed) {
+        keyGenerator = new Random();
+
+        if (seed instanceof Integer) {
+            keyGenerator.setSeed((int) seed);
+        } else if (seed instanceof String) {
+            keyGenerator.setSeed(((String) seed).hashCode());
+        } else {
+            throw new IllegalArgumentException("Seed duhet të jetë int ose String");
+        }
+    }
+    private byte[] generateKeyStream(int length) {
+        byte[] keyStream = new byte[length];
+        keyGenerator.nextBytes(keyStream);
+        return keyStream;
+    }
+
+    public byte[] encrypt(String plaintext) {
+        byte[] plainBytes = plaintext.getBytes();
+        byte[] keyStream = generateKeyStream(plainBytes.length);
+        byte[] cipherBytes = new byte[plainBytes.length];
+
+        for (int i = 0; i < plainBytes.length; i++) {
+            cipherBytes[i] = (byte) (plainBytes[i] ^ keyStream[i]);
+        }
+
+        return cipherBytes;
+    }
+
+    public String decrypt(byte[] ciphertext) {
+        byte[] keyStream = generateKeyStream(ciphertext.length);
+        byte[] plainBytes = new byte[ciphertext.length];
+
+        for (int i = 0; i < ciphertext.length; i++) {
+            plainBytes[i] = (byte) (ciphertext[i] ^ keyStream[i]);
+        }
+
+        return new String(plainBytes);
+    }
+
 }
+
+
+
+
+
+
+
+
